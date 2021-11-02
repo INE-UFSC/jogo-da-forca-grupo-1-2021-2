0d736f47e9c45cad613ab1c5a2f62e7d0591b4e8
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2021-11-02 19:00:45 -0300

    update 1
    Para vcs terem uma ideia de como está ficando

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# Código de um jogo da forca
+# Fazer os imports
 
+letras_digitadas = []
+tentativas = y
+# Estabelecer o número de tentativas (De acordo com o tamanho da palavra?)
+palavra = x 
+#recebe uma palavra aleatória do banco
+erros = 0
+
+for i in range(tentativas):
+    letra = input("Digite uma letra: ").lower()
+    while (letra not in "abcdefghijlmnopqrstuvwz") or (len(letra) > 1) or (letra in letras_digitadas):
+        letra = input("Digite uma letra: ").lower()
+    letras_digitadas.append(letra)
+
+
+    
+    if erros == tentativas:
+        break
