581ed10d02129087706fba67b9eb2bf7e8c95bf3
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2021-11-02 19:14:17 -0300

    update

--- main.py
+++ main.py
@@ -3,18 +3,20 @@
 
 letras_digitadas = []
 tentativas = y
-# Estabelecer o número de tentativas (De acordo com o tamanho da palavra?)
+# Estabelecer o número de tentativas (Boneco)
 palavra = x 
 #recebe uma palavra aleatória do banco
 erros = 0
 
-for i in range(tentativas):
+while True:
     letra = input("Digite uma letra: ").lower()
-    while (letra not in "abcdefghijlmnopqrstuvwz") or (len(letra) > 1) or (letra in letras_digitadas):
+    while (letra not in "abcdefghijlmnopqrstuvxz") or (len(letra) > 1) or (letra in letras_digitadas):
         letra = input("Digite uma letra: ").lower()
     letras_digitadas.append(letra)
 
-
+    ...
     
     if erros == tentativas:
         break
+
+    
