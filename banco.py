3fb2b90d63a795f26b70e10394a11befbb773794
Lucas Cardozo <themegatroll009@gmail.com>
2021-11-02 19:20:31 -0300

    Create banco.py

---
+++ banco.py
@@ -0,0 +1,5 @@
+#escolhe uma palavra aleatoria da lista e retorna ela
+import random
+def palavra():
+    pal = random.choice(["bailarina","futebol","estátua","pintor","astronauta","amor","cadeira","professor","calculadora","pescador","xadrez","romance","lixo"])
+    return pal
